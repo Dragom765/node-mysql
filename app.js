800821f1bb361ebdf93f923d5b04202d44a4fac9
Dragom765 <01dragom@gmail.com>
2016-06-24 08:53:08 -0400

    Works now
    Added the actual app to make it run

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-var mysql = require("mysql");
+var retrieve = function(table, columns, display) {
+	var mysql = require("mysql");
 
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "Dragom765"
-});
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "Dragom765",
+		database: table
+	});
 
-con.connect( function(){
-	if(err)
-		throw(err);
-	else
-		console.log("Whoa. You did it.");
-});
+	con.connect( function(err){
+		if(err)
+			throw err;
+		else
+			console.log("You're connected.");
+	});
+
+	con.query("SELECT "+columns+" FROM customers", function(err, rows) {
+		if(err)
+			throw(err)
+		else{
+			console.log("Look at all the pretty data:\n");
+			for(var i = 0; i < rows.length; i++){
+				console.log(rows[i][display]);
+			}
+		}
+	});
 
 
-con.end(function(err){
-	
-	
-});
+	con.end(function(err){
+	});
+};
+
+var table = "book_tutorial";
+var columns = "*";
+var display = "cust_email";
+
+retrieve(table, columns, display);
